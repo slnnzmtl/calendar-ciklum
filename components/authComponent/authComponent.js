164d9803d04e4e49ec1d385132e78c8ca465f9c5
matterwaves <marketing.odessa1@gmail.com>
2021-03-01 23:53:23 +0200

    hm-2: users stored in db

--- components/authComponent/authComponent.js
+++ components/authComponent/authComponent.js
@@ -1,8 +1,9 @@
 import authComponent from "./authComponent.html";
-import {participants} from "../../assets/data";
+// import {participants} from "../../assets/data";
 import ComponentsHelper from "../../utils/ComponentsHelper";
 import * as Roles from "../../utils/roles";
 import * as Cookies from "../../utils/cookies";
+import Store from "../../utils/store";
 import { publish } from "../../utils/eventBus";
 import  "./authComponent.scss";
 
@@ -13,31 +14,40 @@ export default class Auth extends HTMLElement {
         super();
 
         this.data = {
-            participants
+            participants: []
         };
     }
 
     connectedCallback() {
+        this.data.participants = Store.users;
+        this.fillUsers(this.select, this.data.participants)
+
         this.appendChild(ComponentsHelper.parseElement(authComponent));
         this.classList.add("auth-wrapper");
         
         this.select = this.querySelector(".auth__select");
         this.button = this.querySelector(".auth__button");
-        
-        ComponentsHelper.elementMultiplier('option', ['value'], this.select, this.data.participants.map(item => item.name)); 
+
+        this.button.onclick = () => this.confirm(this.select.value);
+    }
+
+    fillUsers(select, users) {
+        ComponentsHelper.elementMultiplier('option', ['value'], select, users.map(item => item.data.name)); 
         this.select.querySelectorAll("option").forEach(item => {
             item.innerText = item.value;
         })
-
-        this.button.onclick = () => this.confirm(this.select.value);
     }
 
     confirm(user) {
 
         let userObject = this.data.participants.find(item => {
-            return item.name === user;
+            return item.data.name === user;
         });
-        let newUser = userObject.isAdmin ? new Roles.Admin(userObject) : new Roles.User(userObject);
+        let newUser;
+
+        userObject = userObject.data;
+
+        newUser = userObject.isAdmin === "true" ? new Roles.Admin(userObject) : new Roles.User(userObject);
 
         Cookies.setCookie("currentUser", JSON.stringify(newUser));
         
